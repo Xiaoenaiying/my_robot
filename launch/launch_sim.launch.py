39b1c8d3d7c2f3d663534c960f07e21355c412af
Xiaoenaiying <125729896+Xiaoenaiying@users.noreply.github.com>
2026-09-15 18:25:55 +0800

    swap启动

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -28,9 +28,16 @@ def generate_launch_description():
         ])
     )
 
+    swap=Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic','robot_description','-entity','my_robot'],
+        output='screen'
+    )
+
 
     return LaunchDescription([
         rsp,
-        gazebo
-        
+        gazebo,
+        swap
     ])
